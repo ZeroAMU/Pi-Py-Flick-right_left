5167fa8ed0a4de08b9760bfbcc378342372c6459
ZeroAMU <61517169+ZeroAMU@users.noreply.github.com>
2020-02-27 01:03:41 +0530

    Update leftright.py

--- leftright.py
+++ leftright.py
@@ -12,7 +12,7 @@ while True :
 		elif (start == 'north' and finish == 'south'):
 			time.sleep(1)
 			keyboard.press_and_release('down')
-        elif (start == 'east' and finish == 'west'):
+		elif (start == 'east' and finish == 'west'):
 			time.sleep(1)
 			keyboard.press_and_release('left')
 		elif (start == 'west' and finish == 'east'):
