ccad239763c0487154a210d9c71e752fd16cf554
ZeroAMU <61517169+ZeroAMU@users.noreply.github.com>
2020-02-27 00:30:25 +0530

    Create leftright.py

---
+++ leftright.py
@@ -0,0 +1,20 @@
+import keyboard
+import signal
+import flicklib
+import time
+
+while True :
+	@flicklib.flick()
+	def flick(start,finish):
+		if (start == 'south' and finish == 'north'):
+			time.sleep(1)
+			keyboard.press_and_release('up')
+		elif (start == 'north' and finish == 'south'):
+			time.sleep(1)
+			keyboard.press_and_release('down')
+        elif (start == 'east' and finish == 'west'):
+			time.sleep(1)
+			keyboard.press_and_release('left')
+		elif (start == 'west' and finish == 'east'):
+			time.sleep(1)
+			keyboard.press_and_release('right')
