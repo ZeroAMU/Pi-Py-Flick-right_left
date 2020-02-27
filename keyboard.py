a157c0e2d712745674ab0c0163e5deba3701bc8e
ZeroAMU <61517169+ZeroAMU@users.noreply.github.com>
2020-02-27 23:58:53 +0530

    Create keyboard.py

---
+++ keyboard.py
@@ -0,0 +1,13 @@
+import keyboard
+import signal
+import flicklib
+import time
+
+while True :
+        @flicklib.move()
+        def move(x, y, z):
+                keyboard_matrix=[['a','b','c','d','e','f','g','h','i','j'],['k','l','m','n','o','p','q','r','s','t'],['u','v','w','x','y','z','shift',',','.','space'],['1','2','3','4','5','6','7','8','9','0']]
+                x_coord = int(x/.100)
+                y_coord = int(y/.250)
+                keyboard.press_and_release(keyboard_matrix[x_coord][y_coord])
+                time.sleep(1)
